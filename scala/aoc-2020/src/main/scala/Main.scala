0513afc7c753a09e1a994972529446ad10272347
Ben List <ben.list@tangramflex.com>
2020-12-02 10:15:25 -0500

    day 02 + cleanup * moves clojure project into clojure dir * adds scala dir * solves day 02 in scala

---
+++ scala/aoc-2020/src/main/scala/Main.scala
@@ -0,0 +1,12 @@
+package aoc2020
+
+
+trait day {
+    def p1 : Unit
+    def p2 : Unit
+}
+
+object Main extends App {
+  solutions.day02.p1
+  solutions.day02.p2
+}
---
+++ scala/aoc-2020/src/main/scala/solutions/day-02.scala
@@ -0,0 +1,42 @@
+package aoc2020.solutions
+
+import scala.io.Source
+import fastparse._
+import fastparse.Parsed.Failure
+import aoc2020.day
+import fastparse.NoWhitespace._
+
+object day02 extends day {
+  def CHAR[_:P] = CharIn("A-Za-z").!.map(_.toCharArray().head)
+  def DIGITS[_:P] = CharsWhileIn("0-9",1).!.map(_.toInt)
+  def RULE[_:P] = P(DIGITS ~~ "-" ~~ DIGITS ~~ " " ~~ CHAR ~~ ":" ~~ " ")
+  
+  def btwn(v:Int, min:Int, max :Int) = v >= min && v <= max
+  def p1 {
+    def PASS_P1[_:P] = P(RULE ~~ CharsWhileIn("a-zA-Z").! ~~ End)
+    val input = Source.fromFile("../../resources/day-02/input.txt").getLines.toList
+    val valid = input.map(line => parse(line,PASS_P1(_)))
+                     .collect { 
+                         case Parsed.Success((min,max,c,pass),_) if btwn(pass.count(_ == c),min,max) => ()
+                     }
+                    .length
+    println(s"There are $valid passwords")
+  }
+
+  def p2 {
+    def PASS_P2[_:P] = for {
+        (f,s,c) <- RULE
+        _       <- CHAR.rep(f-1,max=f-1)
+        r       <- CHAR
+        _       <- CHAR.rep(s-f-1,max=s-f-1)
+        _       <- if (r == c) CharPred(_ != c) else CharPred(_ == c)
+        _       <- CHAR.rep ~~ End
+    } yield ()
+    val input = Source.fromFile("../../resources/day-02/input.txt").getLines.toList
+    val valid = input.map(line => parse(line,PASS_P2(_)))
+                     .collect { case Parsed.Success(_,i) => () }
+                     .length
+    println(s"There are $valid valid passwords")
+  }
+  
+}
