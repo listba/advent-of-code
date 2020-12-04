36b2fe08323a7f8e9bf01a02517059c46ee2bbd2
Ben List <ben.list@tangramflex.com>
2020-12-03 23:19:17 -0500

    added colors for the hell of it

--- clojure/src/aoc_2020/core.clj
+++ clojure/src/aoc_2020/core.clj
@@ -1,20 +1,24 @@
 (ns aoc-2020.core
-  (:require 
-    [aoc-2020.days.01] [aoc-2020.days.02] 
-    [aoc-2020.days.03])
+  (:require
+   [clojure.term.colors :as colors]
+   [aoc-2020.days.01] [aoc-2020.days.02]
+   [aoc-2020.days.03] [aoc-2020.days.04])
   (:gen-class))
+
 
 (defn run 
   [day part args]
     (let [stmnt (str "aoc-2020.days."day"/p"part)]
-      (println (str "-------------- DAY "day" Part "part" --------------\n"))
-      (println (time (-> stmnt read-string eval (apply args))))))
+      (println (str "-------------- "(colors/cyan (str "DAY " day " Part "part)) "--------------"))
+      (println  "Logs: ")
+      (println (str (colors/bold "Result: ") (colors/green (time (-> stmnt read-string eval (apply args))))))))
 
 (defn -main
   ([] (time (do 
         (->> [["01" 1] ["01" 2]
               ["02" 1] ["02" 2]
-              ["03" 1] ["03" 2]]
+              ["03" 1] ["03" 2]
+              ["04" 1] ["04" 2]]
             (map #(apply -main %))
             (seq)) (println "-------------- TOTAL --------------"))))
   ([day part & args] (run day part args)))
---
+++ clojure/src/aoc_2020/days/04.clj
@@ -0,0 +1,7 @@
+(ns aoc-2020.days.04)
+
+(defn p1
+  [])
+
+(defn p2
+  [])
