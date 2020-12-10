88756f682c8bb53bcc5c9184f737595d92691477
Ben List <ben.list@tangramflex.com>
2020-12-10 13:48:13 -0500

    cleaned up day 10 p2 linear

--- clojure/src/aoc_2020/days/10.clj
+++ clojure/src/aoc_2020/days/10.clj
@@ -12,11 +12,11 @@
   ([file] 
    (->> (parse file)
         (reduce (fn [[p _1 _2 _3] c]
-                  (let [diff (- c p)]
-                    (cond
-                      (= 1 diff) [c (inc _1) _2 _3]
-                      (= 2 diff) [c _1 (inc _2) _3]
-                      (= 3 diff) [c _1 _2 (inc _3)]))) [0 0 0 1]) ;; initilize the _3 count to account for your special adapter of max+3
+                  (case (- c p)
+                    1 [c (inc _1) _2 _3]
+                    2 [c _1 (inc _2) _3]
+                    3 [c _1 _2 (inc _3)])) 
+                [0 0 0 1]) ;; initilize the _3 count to account for your special adapter of max+3
         ((fn [[_ _1 _ _3]] (* _1 _3))))))
 
 (def count-configs-recur 
@@ -29,17 +29,15 @@
 
 (defn count-configs-linear 
   [nums]
-  (->> (reverse nums)
-       (reduce (fn [m c]
-                 (assoc m c (+ (get m (+ 1 c) 0)
-                               (get m (+ 2 c) 0)
-                               (get m (+ 3 c) 0)))) 
-               {(+ 3 (last nums)) 1})   ;; initialize map to include your max+3 adapter as the end with a value of 1
-       (#(get % 0)))) 
+  (-> (reduce (fn [m c]
+                 (assoc m c (+ (get m (- c 1) 0)
+                               (get m (- c 2) 0)
+                               (get m (- c 3) 0)))) 
+               {0 1} nums)   ;; initialize map to include base case of 0
+       (get (last nums)))) 
 
 (defn p2
   ([] (p2 "input"))
   ([file] 
    (-> (parse file)
-       (conj 0)
        (count-configs-linear))))
