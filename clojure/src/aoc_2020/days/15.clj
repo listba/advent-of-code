8c2a306ca7af219a034e59bf632073f7bf5f9720
Ben List <ben.list@tangramflex.com>
2020-12-16 13:42:03 -0500

    some minor per improvements for day 15 down to ~40s from ~60

--- clojure/src/aoc_2020/days/15.clj
+++ clojure/src/aoc_2020/days/15.clj
@@ -4,22 +4,22 @@
   (->> input (re-seq #"\d+") (map-indexed (fn [i x] [(read-string x) i]))))
 
 (defn game
-  ([input turns] (let [nums        (parse input)
-                 spoken      (reduce (fn [m [n i]] (assoc m n  (list (inc i)))) {} nums)
-                 last-spoken (first (last nums))
-                 rng       (range (inc (count nums)) (inc turns))
-                 result (reduce (fn [[spoken last] turn] 
-                                  (let [[x y] (get spoken last)]
-                                    (if (nil? y) [(update spoken 0 #(conj % turn)) 0]
-                                        (let [speak (- x y)
-                                              _ (if (= 0 (mod turn (/ turns 10))) (println (str "turn " turn " last " last " x,y" [x y])))]
-                                          [(update spoken speak #(conj % turn)) speak])))) [spoken last-spoken] rng)] 
+  ([input turns] (let [nums   (parse input)
+                       spoken (reduce (fn [m [n i]] (assoc m n  (inc i))) {} nums)
+                       rng    (range (inc (count nums)) turns)
+                       result (reduce (fn [[spoken heard] turn]
+                                        (let [x (get spoken heard)
+                                              spoken (assoc spoken heard turn)]
+                                          (if (nil? x) [spoken 0]
+                                              (let [speak (- turn x)
+                                                    _ (if (= 0 (mod turn (/ turns 10))) (println (str "turn " turn " heard " heard " x " x)))]
+                                                [spoken speak])))) [spoken 0] rng)] 
                    (second result))))
 
 (defn p1
   ([] (p1 "2,20,0,4,1,17"))
-  ([input] (game input 2020)))
+  ([input] (game input 2020))) ; 758
 
 (defn p2
   ([] (p2 "2,20,0,4,1,17"))
-  ([input] (game input 30000000)))
+  ([input] (game input 30000000))) ; 814
