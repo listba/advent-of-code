aa67a1aa85e2ac753104c1001611c2c78042b508
Ben List <ben.list@tangramflex.com>
2021-12-06 11:45:41 -0500

    solution was to naive / slow for part 2 so refactored it and solved part 2sldl

--- 2021/clojure/src/aoc_2021/days/06.clj
+++ 2021/clojure/src/aoc_2021/days/06.clj
@@ -1,29 +1,28 @@
 (ns aoc-2021.days.06 
   (:require [aoc-2021.util :as util]))
 
-
-(defn simulate-fish [fish] (if (= 0 fish) [6 8] [(dec fish)]))
-
-
-(defn fish-cycle [fishes] 
-  (->> fishes
-        (map simulate-fish)
-        flatten))
-
-
 (defn iterate* [f x] (reductions (fn [dx _] (f dx)) x (range)))
 
+(defn parse-fishes [file]
+  (->> (util/read-file "06" file)
+       (re-seq #"\d+")
+       (map read-string)
+       frequencies))
+
+(defn simulate-fish [fish-map [age count]]
+  (if (= 0 age)
+    (-> fish-map (assoc 8 count) (update 6 (fnil (partial + count) 0)))
+    (assoc fish-map (dec age) count)))
+
+(defn fish-cycle [fish-map]
+  (->> fish-map 
+  (sort-by key #(> %1 %2))
+  (reduce simulate-fish {})))
 
 (defn p1 
   ([] (p1 "input"))
-  ([file] (->> (util/read-file "06" file)
-                (re-seq #"\d+")
-                (map read-string)
-                (iterate* fish-cycle)
-                (take 81)
-                last
-                count)))
+  ([file] (->> (parse-fishes file) (iterate* fish-cycle) (take 81) last vals (apply +))))
   
 (defn p2 
   ([] (p2 "input"))
-  ([file] ))
+  ([file] (->> (parse-fishes file) (iterate* fish-cycle) (take 257) last vals (apply +))))
