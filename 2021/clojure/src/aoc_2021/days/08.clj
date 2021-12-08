184c1fd9cbbf961335dd1b2fea4f1b57bdfca516
Ben List <ben.list@tangramflex.com>
2021-12-08 13:26:44 -0500

    removed some debugging statements + minor refactoring

--- 2021/clojure/src/aoc_2021/days/08.clj
+++ 2021/clojure/src/aoc_2021/days/08.clj
@@ -15,18 +15,6 @@
   ([file] (let [length-count (->> file parse (map last) flatten (mapv count) frequencies)]
             (->> (select-keys length-count [7 4 3 2]) vals (reduce +)))))
 
-
-    ;; acedgfb: 8
-    ;; cdfbe: 5
-    ;; gcdfa: 2
-    ;; fbcad: 3
-    ;; dab: 7
-    ;; cefabd: 9
-    ;; cdfgeb: 6
-    ;; eafb: 4
-    ;; cagedb: 0
-    ;; ab: 1
-
 (defn map-digits [input]
   (let [d1 (set (first (get input 2)))
         d7 (set (first (get input 3)))
@@ -37,20 +25,13 @@
         {[d9] true _6s false} (group-by (partial clojure.set/subset? d4) _6s)
         {[d0] true [d6] false} (group-by (partial clojure.set/subset? d1) _6s)
         {[d5] true _5s false} (group-by (partial clojure.set/superset? d6) _5s)
-        {[d3] true [d2] false} (group-by (partial clojure.set/subset? d1) _5s)
-        _ (println "--------------------------------------")
-        _ (println input)
-        _ (println (str "1: " (reduce str d1) " 2: " (reduce str d2) " 4: " (reduce str d4) " 8: " (reduce str d8)))
-        _ (println (str "9: " (reduce str d9) " 0: " (reduce str d0) " 6: " (reduce str d6)))
-        _ (println (str "5: " (reduce str d5) " 3: " (reduce str d3) " 2: " (reduce str d2)))
-        ]
+        {[d3] true [d2] false} (group-by (partial clojure.set/subset? d1) _5s)]
         {d1 1 d2 2 d3 3 d4 4 d5 5 d6 6 d7 7 d8 8 d9 9 d0 0}))
 (defn match-output [digit-map output]
-(->> output util/p (mapv (partial get digit-map)) (apply str) Long/parseLong))
+(->> output (map set) (map (partial get digit-map)) (apply str) Long/parseLong))
 
-(defn p2 
+(defn p2
   ([] (p2 "input"))
   ([file] (let [data (parse file)]
-            (reduce (fn [total [input output]] 
-            (let [digit-map (map-digits input)]
-            (+ total (match-output digit-map (map set output))))) 0 data))))
+            (reduce (fn [total [input output]]
+                      (-> input map-digits (match-output output) (+ total))) 0 data))))
