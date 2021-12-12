95d98b98d2e9226912443bb51d6dfe34da03acdb
Ben List <ben.list@tangramflex.com>
2021-12-12 13:19:54 -0500

    clojure day 12 part 2 + some refactoring of part 1

--- 2021/clojure/src/aoc_2021/days/12.clj
+++ 2021/clojure/src/aoc_2021/days/12.clj
@@ -23,29 +23,40 @@
                      (add-connection e s))) {})
        build-cave-data))
 
-(defn get-connections [{:keys [paths small]} cave visited]
+(defn get-connections-single [{:keys [paths small]} cave visited]
   (let [small-visited (->> visited (filter (partial contains? small)) set)]
     (->> cave
          (get paths)
          (filter #(not (contains? small-visited %))))))
 
 (defn get-paths 
-  ([cave-system] (trampoline get-paths cave-system "start" []))
-  ([cave-system cave seen]
-                 (let [conns (get-connections cave-system cave seen)
-                       seen (conj seen cave)]
+  ([cave-system conn-search] (trampoline get-paths cave-system conn-search "start" []))
+  ([cave-system conn-search cave seen]
+                 (let [seen (conj seen cave)
+                       conns (conn-search cave-system cave seen)]
                    (cond
                      (= cave "end") [seen]
                      (empty? conns) []
-                     :else #(reduce (fn [paths conn] (concat paths (trampoline get-paths cave-system conn seen))) [] conns)))))
+                     :else #(reduce (fn [paths conn] (concat paths (trampoline get-paths cave-system conn-search conn seen))) [] conns)))))
 
 (defn p1 
   ([] (p1 "input"))
   ([file] (-> file
               parse
-              get-paths
+              (get-paths get-connections-single)
               count)))
+
+(defn get-connections-double [{:keys [paths small]} cave visited]
+  (let [small-freq     (->> visited (filter (partial contains? small)) frequencies)
+        max-small-freq (reduce max 0 (vals small-freq))
+        small-visited  (if (= 2 max-small-freq) (-> small-freq keys set) #{})]
+    (->> cave
+         (get paths)
+         (filter #(not (contains? small-visited %))))))
 
 (defn p2 
   ([] (p2 "input"))
-  ([file] ))
+  ([file] (-> file
+              parse
+              (get-paths get-connections-double)
+              count)))
