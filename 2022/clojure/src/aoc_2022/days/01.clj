df5490ce1eff99ecff25305dd42e6da283c2eeaf
Ben List <ben.list@tangramflex.com>
2022-12-02 01:24:35 -0500

    Day 1 & 2

--- 2022/clojure/src/aoc_2022/days/01.clj
+++ 2022/clojure/src/aoc_2022/days/01.clj
@@ -1,10 +1,42 @@
 (ns aoc-2022.days.01 
-  (:require [aoc-2022.util :as util]))
+  (:require [aoc-2022.util :as util]
+            [clojure.string :as str]))
+
+(defn parse-food [file]
+  (let [contents (util/read-file "01" file)
+        elves (str/split contents #"\n\n")]
+    (map #(->> %
+               (re-seq #"\d+")
+               (map read-string)) elves)))
 
 (defn p1 
   ([] (p1 "input"))
-  ([file] ))
+  ([file] 
+   (->> file
+        parse-food
+        (map (partial apply +))
+        (sort)
+        (last))))
 
 (defn p2 
   ([] (p2 "input"))
-  ([file] ))
+  ([file] (->> file
+               parse-food
+               (map (partial apply +))
+               (sort >=)
+               (take 3)
+               (apply +))))
+
+(comment 
+  (->> "input" p1))
+
+(comment 
+  (->> "input" p2))
+
+(comment
+  (->> "input"
+       parse-food
+       (map (partial apply +))
+       (sort >=)
+       (take 3)
+       (apply +)))
--- 2022/clojure/src/aoc_2022/days/02.clj
+++ 2022/clojure/src/aoc_2022/days/02.clj
@@ -1,11 +1,37 @@
 (ns aoc-2022.days.02 
   (:require [aoc-2022.util :as util]))
 
+; A Rock ; B Paper ; C Scissors
+; X Rock ; Y Paper ; Z Scissors
+(def match-score-p1 {"A" {"X" 4 "Y" 8 "Z" 3}
+                     "B" {"X" 1 "Y" 5 "Z" 9}
+                     "C" {"X" 7 "Y" 2 "Z" 6}})
+
+; A:Rock  X->Scissors (L) 0+3; Y->Rock (D) 3+1; Z->Paper (W) 6+2
+; B:Paper X->Rock (L) 0+1; Y->Paper (D) 3+2; Z->Scissors (W) 6+1
+; C:Scissors X->Paper (L) 0+2; Y->Scissors (D) 3+3; Z->Rock (W) 6+1
+(def match-score-p2 {"A" { "X" 3 "Y" 4 "Z" 8}   
+                     "B" { "X" 1  "Y" 5 "Z" 9}
+                     "C" { "X" 2 "Y" 6 "Z" 7}})
+
+(defn parse [file] (->> file
+                        (util/read-file "02")
+                        (re-seq #"\w")
+                        (partition 2)))
 
 (defn p1 
   ([] (p1 "input"))
-  ([file] ))
+  ([file] (->> file
+           parse
+           (map (fn [[op me]] ((match-score-p1 op) me)))
+           (apply +))))
 
 (defn p2 
   ([] (p2 "input"))
-  ([file] ))
+  ([file] (->> file
+           parse
+           (map (fn [[op me]] ((match-score-p2 op) me)))
+           (apply +))))
+
+(comment (p1))
+(comment (p2))
