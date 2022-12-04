ce4189ca78f5da254103a0279c1c6ceeba70cede
Ben List <ben.list@tangramflex.com>
2022-12-04 00:53:28 -0500

    Day 04 solution

--- 2022/clojure/src/aoc_2022/days/04.clj
+++ 2022/clojure/src/aoc_2022/days/04.clj
@@ -3,10 +3,30 @@
             [clojure.string :as str]
             [clojure.set]))
 
+(defn parse [file]
+  (->> file
+       (util/parse-lines "04")
+       (mapv #(->> % (re-seq #"\d+") (mapv read-string)))))
+
 (defn p1 
   ([] (p1 "input"))
-  ([file] ))
+  ([file] (->> file
+               parse
+               (filter (fn [[a b x y]]
+                         (or
+                          (and (<= a x) (>= b y))
+                          (and (<= x a) (>= y b)))))
+               (count))))
 
 (defn p2 
   ([] (p2 "input"))
-  ([file] ))
+  ([file] (->> file
+               parse
+               (filter (fn [[a b x y]]
+                         (or
+                          (and (<= a x) (>= b x))
+                          (and (<= x a) (>= y a)))))
+               (count))))
+
+
+(str "p1: " (p1) " p2: " (p2))
