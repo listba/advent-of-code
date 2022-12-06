aa90f99f2725a40451f6a04a561b1c7867adf33c
Ben List <ben.list@tangramflex.com>
2022-12-06 01:04:19 -0500

    day 05 & day 05

--- 2022/clojure/src/aoc_2022/days/05.clj
+++ 2022/clojure/src/aoc_2022/days/05.clj
@@ -1,10 +1,60 @@
 (ns aoc-2022.days.05 
-  (:require [aoc-2022.util :as util]))
+  (:require [aoc-2022.util :as util]
+            [clojure.string :as str]))
+
+(defn process-crate [[col crate] crate-map] 
+  (update crate-map col #(conj % crate)))
+
+(defn index-crates [row] (keep-indexed (fn [idx c]
+                       (if (and (<= (int \A) (int c)) (>= (int \Z) (int c))) [(-> idx (quot 4) (+ 1)) c] nil)) 
+                       row))
+
+(defn process-row [row crate-map]
+  (->> row index-crates (reduce (fn [crate-map crate] (process-crate crate crate-map)) crate-map)))
+
+(defn generate-crate-map [[axis & crates]]
+  (let [initial-map (->> axis (re-seq #"\d+") (map read-string) (reduce (fn [b i] (conj b (sorted-map i []))) (sorted-map)))]
+    (reduce (fn [crate-map row] (process-row row crate-map)) initial-map crates)))
+
+(defn parse [file]
+  (->> file
+       (util/split-blank-lines "05")
+       (map (partial re-seq #"[^\n]+"))
+       (#(let [[crate-chart raw-moves] %
+               crate-map (generate-crate-map (reverse crate-chart))
+               moves (map (fn [move] ( ->> move (re-seq #"\d+") (map read-string))) raw-moves)] 
+           [crate-map moves]))))
+
+(defn pull-crates [from [crate-map crates]]
+  (reduce (fn [[crate-map crates] crate-idx]
+            [(update crate-map crate-idx pop) 
+             (->> [crate-idx] (get-in crate-map) last (conj crates))]) 
+          [crate-map []] (repeat crates from)))
+
+(defn push-crates [multi-crate? to [crate-map crates]]
+  (reduce (fn [crate-map crate]
+            (update crate-map to #(conj % crate))) crate-map (if multi-crate? (reverse crates) crates)))
+
+(defn move-crates [multi-crate? crate-map moves]
+  (reduce (fn [crate-map [crates from to]]
+            (->> [crate-map crates]
+                 (pull-crates from)
+                 (push-crates multi-crate? to))) crate-map moves))
 
 (defn p1 
   ([] (p1 "input"))
-  ([file] ))
+  ([file] (let [[crate-map moves] (parse file)]
+            (->> (move-crates false crate-map moves)
+                 (vals)
+                 (map last)
+                 (apply str)))))
 
 (defn p2 
   ([] (p2 "input"))
-  ([file] ))
+  ([file] (let [[crate-map moves] (parse file)]
+            (->> (move-crates true crate-map moves)
+                 (vals)
+                 (map last)
+                 (apply str)))))
+
+(str "p1: " (p1) " p2: " (p2))
--- 2022/clojure/src/aoc_2022/days/06.clj
+++ 2022/clojure/src/aoc_2022/days/06.clj
@@ -1,10 +1,19 @@
     (ns aoc-2022.days.06 
       (:require [aoc-2022.util :as util]))
-    
+
+(defn first-unique [size signal]
+  (->> signal 
+       (partition size 1)
+       (map set)
+       (keep-indexed (fn [idx window] (if (== size (count window)) [idx window] nil)))
+       first))
+
 (defn p1 
   ([] (p1 "input"))
-  ([file] ))
+  ([file] (->> file (util/read-file "06") (first-unique 4) first (+ 4))))
 
 (defn p2 
   ([] (p2 "input"))
-  ([file] ))
+  ([file] (->> file (util/read-file "06") (first-unique 14) first (+ 14))))
+
+(str "p1: " (p1) " p2: " (p2))
